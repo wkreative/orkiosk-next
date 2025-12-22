9ac8b5c23574547ceabb0c0ca9805862638973b7
wkreative <josejimenezmorales@hotmail.com>
2025-12-22 11:24:57 -0400

    Create Home component for main page layout

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ height: '100vh', display: 'grid', placeContent: 'center', background: '#f5f7fa', color: '#0066ff' }}>
+      <h1>Orkiosk – Nueva web en camino</h1>
+      <p>Próximamente con Next.js + TinaCMS</p>
+    </main>
+  )
+}
